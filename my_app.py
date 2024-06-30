c3c1b1fc27b513215bfd7582d7378e397790d6df
Pasha <ssd@gmail.com>
2024-06-30 17:42:41 +0300

    version3

--- my_app.py
+++ my_app.py
@@ -1,1 +1,45 @@
 # напиши тут код основної програми та першого екрану
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout, QLineEdit
+
+import second_win, final_win
+
+app = QApplication([])
+main_win = QWidget()
+main_win.setWindowTitle("Здоров'я")
+main_win.move(900, 70)
+main_win.resize(1000, 600)
+layout = QVBoxLayout()
+
+
+def next_page():
+    app.closeAllWindows()
+    second_win.main_win1.show()
+    second_win.app1.exec_()
+
+
+def last_page():
+    second_win.app1.closeAllWindows()
+    final_win.main_win2.show()
+    final_win.app2.exec_()
+    second_win.main_win1.show()
+    second_win.app1.exec_()
+
+
+text1 = QLabel("Ласкаво просимо до програми визначення стану здоров'я!")
+text2 = QLabel("Ця програма дозволить вам за допомогою тесту Руф'є провести первинну діагностику вашого здоров'я. \n"
+               "Проба Руф'є є навантажувальним комплексом, призначеним для оцінки працездатності серця при фізичному "
+               "навантаженні. \n У випробуваного, що знаходиться в положенні лежачи на спині протягом 5хв, визначають "
+               "частоту пульсу за 15с; \n потім протягом 45с випробуваний виконує 30 присідань. \n Після закінчення "
+               "Навантаження випробуваний лягає, і в нього знову підраховується кількість пульсацій за перші 15с, \n"
+               "Важливо! Якщо в процесі проведення випробовування ви відчуваєте себе погано(з'явиться запаморочення, "
+               "шум \n у вухах, сильна задишка та ін.), то тест необхідно перервати і звернутися до лікаря.")
+button = QPushButton("Почати")
+button.pressed.connect(next_page)
+second_win.next_button.pressed.connect(last_page)
+layout.addWidget(text1, alignment=Qt.AlignLeft)
+layout.addWidget(text2, alignment=Qt.AlignLeft)
+layout.addWidget(button, alignment=Qt.AlignCenter)
+main_win.setLayout(layout)
+main_win.show()
+app.exec_()
--- second_win.py
+++ second_win.py
@@ -1,1 +1,49 @@
 # напиши тут код для другого екрана програми
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout, QLineEdit, QHBoxLayout
+
+app1 = QApplication([])
+main_win1 = QWidget()
+main_win1.setWindowTitle("Здоров'я")
+main_win1.move(900, 70)
+main_win1.resize(1000, 600)
+layout1 = QVBoxLayout()
+layout2 = QVBoxLayout()
+main_layout = QHBoxLayout()
+
+text1 = QLabel("Введіть П.І.Б.:")
+PIB_input = QLineEdit("П.І.Б.")
+text2 = QLabel("Пвних років:")
+age_input = QLineEdit("0")
+text3 = QLabel("Ляжте на спину і заміряйте пульс за 15с. Натисніть на копку 'Почати перший тест', щоб запустити "
+               "таймер. \n Результат запишіть у відповідне поле.")
+first_button = QPushButton("Почати перший тест")
+first_pulse_input = QLineEdit("0")
+text4 = QLabel("Виконайте 30 присідань за 45с. Для цього натисніть на кнопку 'Почати робити присідання', щоб запустити "
+               "лічильник присідань.")
+second_button = QPushButton("Почати робити присідання")
+text5 = QLabel("Лягте на спину і заміряйте пульс спочатку за перші 15с хвилини, потім за останні 15с хвилини без виміру"
+               " пульсацій. Результати запишіть у відповідні поля")
+final_button = QPushButton("Почати фінальний тест")
+second_pulse_input = QLineEdit("0")
+final_pulse_input = QLineEdit("0")
+next_button = QPushButton("Надіслати результати")
+timer = QLabel()
+
+layout1.addWidget(text1, alignment=Qt.AlignLeft)
+layout1.addWidget(PIB_input, alignment=Qt.AlignLeft)
+layout1.addWidget(text2, alignment=Qt.AlignLeft)
+layout1.addWidget(age_input, alignment=Qt.AlignLeft)
+layout1.addWidget(text3, alignment=Qt.AlignLeft)
+layout1.addWidget(first_button, alignment=Qt.AlignLeft)
+layout1.addWidget(first_pulse_input, alignment=Qt.AlignLeft)
+layout1.addWidget(text4, alignment=Qt.AlignLeft)
+layout1.addWidget(second_button, alignment=Qt.AlignLeft)
+layout1.addWidget(text5, alignment=Qt.AlignLeft)
+layout1.addWidget(first_button, alignment=Qt.AlignLeft)
+layout1.addWidget(second_pulse_input, alignment=Qt.AlignLeft)
+layout1.addWidget(final_pulse_input, alignment=Qt.AlignLeft)
+layout1.addWidget(next_button, alignment=Qt.AlignCenter)
+main_layout.addLayout(layout1)
+main_win1.setLayout(main_layout)
+
