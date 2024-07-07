27ecc35f2dbeaa6edea6e786be13f9c1983be2cd
Pasha <ssd@gmail.com>
2024-07-07 19:02:05 +0300

    0.5

--- my_app.py
+++ my_app.py
@@ -1,14 +1,44 @@
 # напиши тут код основної програми та першого екрану
+from PyQt5.QtCore import Qt, QTime, QTimer
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout, QHBoxLayout, QLineEdit
-
-from second_win import TestWin
 
 app = QApplication([])
 
+from second_win import *
 
-win2 = TestWin()
-win2.set_appear()
-win2.initUI()
-win2.show()
+
+class MainWin(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()  # створюємо та налаштовуємо графічні елементи
+        self.connects()  # Встановлює зв'язки між елементами
+        self.set_appear()  # Встановлює, як виглядатиме вікно (напис, розмір, місце)
+        self.show()  # старт
+
+    def set_appear(self):
+        self.setWindowTitle(txt_title)
+        self.resize(win_width, win_height)
+        self.move(win_x, win_y)
+
+    def initUI(self):
+        self.btn_next = QPushButton(txt_next)
+        self.hello_text = QLabel(txt_hello)
+        self.instruction = QLabel(txt_instruction)
+
+        self.layout_line = QVBoxLayout()
+        self.layout_line.addWidget(self.hello_text, alignment=Qt.AlignLeft)
+        self.layout_line.addWidget(self.instruction, alignment=Qt.AlignLeft)
+        self.layout_line.addWidget(self.btn_next, alignment=Qt.AlignCenter)
+        self.setLayout(self.layout_line)
+
+    def next_click(self):
+        self.tw = TestWin()
+        self.hide()
+
+    def connects(self):
+        self.btn_next.clicked.connect(self.next_click)
+
+
+win1 = MainWin()
 
 app.exec_()
--- second_win.py
+++ second_win.py
@@ -2,10 +2,9 @@
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QWidget, QPushButton, QLabel, QLineEdit, QVBoxLayout, QHBoxLayout
 from instr import *
+from final_win import FinalWin
 
 second_win = QWidget()
-
-
 
 
 class TestWin(QWidget):
@@ -16,52 +15,55 @@ class TestWin(QWidget):
         self.show()
 
     def set_appear(self):
-        second_win.setWindowTitle("Здоров'я")
-        second_win.move(900, 70)
-        second_win.resize(1000, 600)
+        self.setWindowTitle(txt_title)
+        self.resize(win_width, win_height)
+        self.move(win_x, win_y)
 
-
-    def show(self):
-        second_win.show()
 
     def initUI(self):
-
-        text1 = QLabel(txt_name)
-        PIB_input = QLineEdit(txt_hintname)
-        text2 = QLabel(txt_age)
-        age_input = QLineEdit(txt_hintage)
-        text3 = QLabel(txt_test1)
-        first_button = QPushButton(txt_starttest1)
-        first_pulse_input = QLineEdit(txt_hinttest1)
-        text4 = QLabel(txt_test2)
-        second_button = QPushButton(txt_starttest2)
-        text5 = QLabel(txt_test3)
-        final_button = QPushButton(txt_starttest3)
-        second_pulse_input = QLineEdit(txt_hinttest2)
-        final_pulse_input = QLineEdit(txt_hinttest3)
-        next_button = QPushButton(txt_sendresults)
-        timer = QLabel(txt_timer)
+        self.text1 = QLabel(txt_name)
+        self.PIB_input = QLineEdit(txt_hintname)
+        self.text2 = QLabel(txt_age)
+        self.age_input = QLineEdit(txt_hintage)
+        self.text3 = QLabel(txt_test1)
+        self.first_button = QPushButton(txt_starttest1)
+        self.first_pulse_input = QLineEdit(txt_hinttest1)
+        self.text4 = QLabel(txt_test2)
+        self.second_button = QPushButton(txt_starttest2)
+        self.text5 = QLabel(txt_test3)
+        self.final_button = QPushButton(txt_starttest3)
+        self.second_pulse_input = QLineEdit(txt_hinttest2)
+        self.final_pulse_input = QLineEdit(txt_hinttest3)
+        self.next_button = QPushButton(txt_sendresults)
+        self.timer = QLabel(txt_timer)
 
         self.h_line = QHBoxLayout()
         self.r_line = QVBoxLayout()
         self.l_line = QVBoxLayout()
 
-        self.l_line.addWidget(text1, alignment=Qt.AlignLeft)
-        self.l_line.addWidget(PIB_input, alignment=Qt.AlignLeft)
-        self.l_line.addWidget(text2, alignment=Qt.AlignLeft)
-        self.l_line.addWidget(age_input, alignment=Qt.AlignLeft)
-        self.l_line.addWidget(text3, alignment=Qt.AlignLeft)
-        self.l_line.addWidget(first_button, alignment=Qt.AlignLeft)
-        self.l_line.addWidget(first_pulse_input, alignment=Qt.AlignLeft)
-        self.l_line.addWidget(text4, alignment=Qt.AlignLeft)
-        self.l_line.addWidget(second_button, alignment=Qt.AlignLeft)
-        self.l_line.addWidget(text5, alignment=Qt.AlignLeft)
-        self.l_line.addWidget(final_button, alignment=Qt.AlignLeft)
-        self.l_line.addWidget(first_button, alignment=Qt.AlignLeft)
-        self.l_line.addWidget(second_pulse_input, alignment=Qt.AlignLeft)
-        self.l_line.addWidget(final_pulse_input, alignment=Qt.AlignLeft)
-        self.l_line.addWidget(next_button, alignment=Qt.AlignCenter)
-        self.r_line.addWidget(timer, alignment=Qt.AlignCenter)
+        self.l_line.addWidget(self.text1, alignment=Qt.AlignLeft)
+        self.l_line.addWidget(self.PIB_input, alignment=Qt.AlignLeft)
+        self.l_line.addWidget(self.text2, alignment=Qt.AlignLeft)
+        self.l_line.addWidget(self.age_input, alignment=Qt.AlignLeft)
+        self.l_line.addWidget(self.text3, alignment=Qt.AlignLeft)
+        self.l_line.addWidget(self.first_button, alignment=Qt.AlignLeft)
+        self.l_line.addWidget(self.first_pulse_input, alignment=Qt.AlignLeft)
+        self.l_line.addWidget(self.text4, alignment=Qt.AlignLeft)
+        self.l_line.addWidget(self.second_button, alignment=Qt.AlignLeft)
+        self.l_line.addWidget(self.text5, alignment=Qt.AlignLeft)
+        self.l_line.addWidget(self.final_button, alignment=Qt.AlignLeft)
+        self.l_line.addWidget(self.first_button, alignment=Qt.AlignLeft)
+        self.l_line.addWidget(self.second_pulse_input, alignment=Qt.AlignLeft)
+        self.l_line.addWidget(self.final_pulse_input, alignment=Qt.AlignLeft)
+        self.l_line.addWidget(self.next_button, alignment=Qt.AlignCenter)
+        self.r_line.addWidget(self.timer, alignment=Qt.AlignCenter)
         self.h_line.addLayout(self.l_line)
         self.h_line.addLayout(self.r_line)
-        second_win.setLayout(self.h_line)
+        self.setLayout(self.h_line)
+
+    def next_click(self):
+        self.hide()
+        self.next_button = FinalWin()
+
+    def connects(self):
+        self.btn_next.clicked.connect(self.next_click)
